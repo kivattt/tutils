bc9851d753a2dbe94533780a0d6c3e7049ef8fcd
kivattt <kivattt@gmail.com>
2024-04-20 21:10:42 +0200

    Fix up xxd, check if we're being piped into another program

--- src/xxd.rs
+++ src/xxd.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::cmp;
 use std::io::prelude::*;
 use std::fs::File;
+use std::io::IsTerminal;
 use std::process::{self};
 use clap::Parser;
 
@@ -10,7 +11,7 @@ use clap::Parser;
 struct Args {
     files: Vec<String>,
 
-    #[arg(long, default_value_t = String::from("always"))] // "always", "never"
+    #[arg(long, default_value_t = String::from("auto"))] // "always", "auto", "never"
     color: String,
 }
 
@@ -20,18 +21,18 @@ const GROUP_BYTES: usize = 2;
 
 fn color_of_byte(b: u8) -> String {
     if !b.is_ascii() {
-        return "\x1b[0;91m".to_string();
+        return "\x1b[1;31m".to_string();
+    }
+
+    if b == b' ' {
+        return "\x1b[1;34m".to_string();
     }
 
     if b.is_ascii_whitespace() {
-        return "\x1b[0;93m".to_string();
+        return "\x1b[1;33m".to_string();
     }
 
-    if b.is_ascii() {
-        return "\x1b[0;92m".to_string();
-    }
-
-    return "".to_string();
+    return "\x1b[1;32m".to_string();
 }
 
 fn should_print_dot(b: u8) -> bool {
@@ -72,16 +73,12 @@ fn handle_buf(buf: [u8; 512], size: usize, index: usize, color: &String) -> bool
             }
         }
 
-        if i % GROUP_BYTES != 0 {
-            print!(" ");
-        }
+        print!(" ");
 
-//        let whitespace = String::from(" ").repeat(2*BYTES_PER_LINE + BYTES_PER_LINE / GROUP_BYTES);
-//        let whitespace = String::from(" ").repeat(2*(BYTES_PER_LINE - i) + (i - i/GROUP_BYTES));
-//        let whitespace = String::from(" ").repeat(2*(BYTES_PER_LINE - i) * (i/GROUP_BYTES));
-//        let whitespace = String::from(" ").repeat(2*BYTES_PER_LINE - n_bytes_printed);
-//        let whitespace = String::from(" ").repeat((BYTES_PER_LINE - i) * (i / GROUP_BYTES) * 2);
-        let whitespace = String::from(" ").repeat((BYTES_PER_LINE - i) * 2);
+        let n_expected = 2*BYTES_PER_LINE + ((BYTES_PER_LINE / GROUP_BYTES) as f64).ceil() as usize;
+        let n_bytes_printed = 2*i + ((i / GROUP_BYTES) as f64).ceil() as usize;
+
+        let whitespace = String::from(" ").repeat(n_expected - n_bytes_printed);
         print!("{}", whitespace);
 
         for b in &buf[line_index..cmp::min(size, line_index+BYTES_PER_LINE)] {
@@ -102,31 +99,19 @@ fn handle_buf(buf: [u8; 512], size: usize, index: usize, color: &String) -> bool
 
         println!();
     }
-/*
-    let mut i = index;
-    print!("{}", format!("{:#010X}: ", i).to_string().strip_prefix("0x").unwrap());
-    for b in &buf[..size] {
-        print!("{}{}", HEX_LOOKUP[(b>>4) as usize] as char, HEX_LOOKUP[(b & 0xf) as usize] as char);
-        i += 1;
-        if i % 2 == 0 {
-            print!(" ");
-        }
-
-        if i % BYTES_PER_LINE == 0 {
-            println!();
-            print!("{}", format!("{:#010X}: ", i).to_string().strip_prefix("0x").unwrap());
-        }
-    }
-
-    if i > index {
-        println!();
-    }*/
 
     return true;
 }
 
 fn main() -> io::Result<()> {
-    let args = Args::parse();
+    let mut args = Args::parse();
+
+    // Don't output color if we're being piped into another program
+    if args.color != "always" {
+        if !std::io::stdout().is_terminal() {
+            args.color = "never".to_string();
+        }
+    }
 
     // No input files? Read from STDIN
     if args.files.is_empty() {
@@ -163,7 +148,7 @@ fn main() -> io::Result<()> {
             }
         } else {
             if args.color != "never" {
-                eprintln!("\x1b[01;31mNo such file or directory: {}\x1b[0m", file);
+                eprintln!("\x1b[1;31mNo such file or directory: {}\x1b[0m", file);
             } else {
                 eprintln!("No such file or directory: {}", file);
             }
